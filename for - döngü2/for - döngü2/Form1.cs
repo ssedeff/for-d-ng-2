7d5985afd84cb58d42abec32e81b230974113962
ssedeff <145463803+ssedeff@users.noreply.github.com>
2023-10-23 14:38:14 +0300

    Add files via upload

---
+++ for - döngü2/for - döngü2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for___döngü2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int toplam = 0;
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            for(int i =sayi1; i <= sayi2; ++i)
+            {
+                toplam += i;
+            }
+            label1.Text = toplam.ToString();
+
+        }
+    }
+}
